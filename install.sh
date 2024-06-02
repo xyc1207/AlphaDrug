3f6e37aaccda07081e5442a6c1308c8ed0737010
Yingce Xia <yinxia@microsoft.com>
2024-06-02 06:34:24 +0000

    fix installation code

---
+++ install.sh
@@ -0,0 +1,8 @@
+pip install torch torchvision torchaudio
+pip install matplotlib
+pip install graphviz
+pip install loguru
+pip install easydict
+pip install rdkit-pypi
+pip install biopython
+pip install ipykernel
